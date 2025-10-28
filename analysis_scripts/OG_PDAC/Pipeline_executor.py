e64f1af257f1e0648c745f37291ad818a87a7971
JLangecker <langeckerjulian@gmail.com>
2025-10-28 14:54:46 +0100

    run 2.1 infercnvs

--- analysis_scripts/OG_PDAC/Pipeline_executor.py
+++ analysis_scripts/OG_PDAC/Pipeline_executor.py
@@ -35,6 +35,7 @@ RAW_DATA_DIRS = [
                 ]
 OUTPUT_STORAGE_DIR = os.path.join(SCRIPT_DIR, "..", "..", "Data", "output_storage")  # directory for optional permanent storage of indermediate subprocess outputs
 TEMP_DIR = os.path.join(tempfile.gettempdir(),"python") # directory for storage of temporary pipeline files
+AUX_DATA_DIR = os.path.join(SCRIPT_DIR, "..", "..", "auxiliary_data")
 
 
 # variable to determine what intermediate files should be saved permanently, one key per script
@@ -409,7 +410,6 @@ def correct_batch_effects(
     Outputs a gzip compressed corrected h5ad file with batch corrected representations.
     Output files are named {output_prefix}_{basename}.h5ad.
     
-    Annotations added to adata.layers: [dtype(adata.X): "X_log_normalized"] (log1p normalized (total to 1e4) representation of adata.X)
     Annotations added to adata.obsm: [pandas.DataFrame: "X_scVI_corrected", pandas.DataFrame: "X_scANVI_corrected"] (scVI and scANVI corrected normalized representations)
 
     Parameters:
@@ -771,8 +771,8 @@ if __name__ == "__main__": # ensures this code runs only when this script is exe
         # temp_output_files = preprocess_data(RAW_DATA_DIRS[0], mode, use_ensembl_ids=use_ensebml_ids, save_output=False, verbose=True, filtering_params=FilteringParameters(min_n_cells_percentage=0))
         # annotate_cell_types(os.path.join(OUTPUT_STORAGE_DIR, "preprocessed"), use_ensebml_ids, r"C:\Users\Julian\Documents\not_synced\Github\Bachelor_thesis_pipeline\auxiliary_data\annotations\marker_genes.json", r"C:\Users\Julian\Documents\not_synced\Github\Bachelor_thesis_pipeline\auxiliary_data\annotations\negative_markers.json", model="cellassign", verbose=True, save_output=True)
         # output_path_list = aggregate_batches(os.path.join(OUTPUT_STORAGE_DIR, "cell_type_annotated"), save_output=True, verbose=True)
-        correct_batch_effects(os.path.join(OUTPUT_STORAGE_DIR, "aggregated", "aggregated_PDAC.h5ad"), save_output=True, verbose=True, max_considered_genes="all")
-        # infer_CNVs(r"C:\Users\Julian\Documents\not_synced\Github\Bachelor_thesis_pipeline\Data\output_storage\aggregated\aggregated_PDAC.h5ad", r"C:\Users\Julian\Documents\not_synced\Github\Bachelor_thesis_pipeline\auxiliary_data\annotations\gencode.v49.annotation.gtf.gz", save_output=True, input_prefix="aggregated", verbose=True, cell_type="ductal_cell")
+        # correct_batch_effects(os.path.join(OUTPUT_STORAGE_DIR, "aggregated", "aggregated_PDAC.h5ad"), save_output=True, verbose=True, max_considered_genes="all")
+        infer_CNVs(os.path.join(OUTPUT_STORAGE_DIR, "batch_corrected", "batch_corrected_PDAC.h5ad"), os.path.join(AUX_DATA_DIR, "annotations", "gencode.v49.annotation.gtf.gz"), save_output=True, input_prefix="batch_corrected", verbose=True, cell_type="ductal_cell", corrected_representation="X_scANVI_corrected")
         # infer_pseudotime(os.path.join(OUTPUT_STORAGE_DIR, "CNV", "CNV_inferred_PDAC.h5ad"), verbose=True, corrected_representation=None, save_output=True)
         # cluster_and_plot(os.path.join(OUTPUT_STORAGE_DIR, "pseudotime", "pseudotime_inferred_PDAC.h5ad"), ["projections", "pseudotime_vs_cnv"], layers=["X", "X_cnv"], root_cell_idx=14, marker_file_path=r"C:\Users\Julian\Documents\not_synced\Github\Bachelor_thesis_pipeline\auxiliary_data\annotations\marker_genes.json", obs_annotations=["cnv_score", "dpt_pseudotime", "cell_type", "summed_cnvs", "cancer_state", "batch"], cell_type="ductal_cell", projection="UMAP", show=True, save_output=False, verbose=True)
 
